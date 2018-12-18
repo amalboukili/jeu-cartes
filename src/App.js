ce803cbea94b576afc84c12f49ae8a117a3b2edf
Amal Boukili <boukili.amal@gmail.com>
2018-12-18 11:56:53 +0100

    début jeux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import React, { Component } from 'react';
-import './App.css';
-import GuessCount from './GuessCount';
-import Card from './Card';
+import React, { Component } from 'react'
+import shuffle from 'lodash.shuffle'
+
+import './App.css'
+
+import Card from './Card'
+import GuessCount from './GuessCount'
+
+const SIDE = 6
+const SYMBOLS = '🎉😍💖😺🎩🐶🐱🐬🌛💫🍎🍌🍓🍟'
 
 class App extends Component {
+  cards = this.generateCards()
+
+  generateCards() {
+    const result = []
+    const size = SIDE * SIDE
+    const candidates = shuffle(SYMBOLS)
+    while (result.length < size) {
+      const card = candidates.pop()
+      result.push(card, card)
+    }
+    return shuffle(result)
+  }
+
+  handleCardClick(card) {
+    console.log(card, 'clicked')
+  }
+
   render() {
+    const won = new Date().getSeconds() % 2 === 0
     return (
       <div className="memory">
-        <GuessCount guesses={0}/> 
-        <Card card=":-)" feedback="hidden" /> 
-        <Card card=":-D" feedback="justMatched" />
-        <Card card="<3" feedback="justMismatched" />
-        <Card card=":-P" feedback="visible" />
-        <Card card=":-S" feedback="visible" />
-        <Card card=":-(" feedback="justMatched" />
-         
+        <GuessCount guesses={0} />
+        <Card card="😍" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick} />
+        <Card
+          card="💖"
+          feedback="justMismatched"
+          onClick={this.handleCardClick}
+        />
+        <Card card="🎩" feedback="visible" onClick={this.handleCardClick} />
+        <Card card="🐶" feedback="hidden" onClick={this.handleCardClick} />
+        <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick} />
+        {won && <p>GAGNÉ !</p>}
       </div>
     )
   }
 }
 
-export default App;
+export default App
